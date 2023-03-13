e47c6e329fd8d69aa3edbf44c81017d2ade8f070
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-03-13 15:09:34 +0100

    gallery setup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+// DATA
+
+// const ul = document.createElement("ul");
+// gallery.append(ul);
+
+galleryItems.forEach((n) => {
+  const image = `<div class="gallery__item">
+  <a href="${n.original}">
+    <img
+      class="gallery__image"
+      src="${n.preview}"
+      alt="${n.description}"
+      data-original="${n.original}"
+  /></a>
+</div>`;
+  gallery.insertAdjacentHTML("beforeend", image);
+});
 console.log(galleryItems);
