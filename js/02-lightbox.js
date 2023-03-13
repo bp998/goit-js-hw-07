b255449de35088bac99c45fda0b51ec55b10acaa
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-03-13 16:33:25 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+// DATA
+
+galleryItems.forEach((n) => {
+  const image = `
+  <a class="gallery__item" href="${n.original}">
+    <img
+      class="gallery__image"
+      src="${n.preview}"
+      alt="${n.description}"
+      data-source="${n.original}"
+  /></a>
+`;
+  gallery.insertAdjacentHTML("beforeend", image);
+});
+
+var lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
